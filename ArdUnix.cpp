364f0e8904ffbf2693d4d95159ad8726a944d009
NINJA-J <1605563934@qq.com>
2017-09-11 17:08:02 +0800

    Update Some Changes
    添加ArdUnix函数执行状态的静态变量
    addApp 添加ArdUnixBase函数
    将BaseBlock作为私有结构体

--- ArdUnix.cpp
+++ ArdUnix.cpp
@@ -2,13 +2,55 @@
 
 ArdUnix::ArdUnix():ArdUnixBase("Base"),HardwareSerial(){
 	head = tail = NULL;
+	sSerialList = (SoftwareSerial**)new *SoftwareSerial[5];
+	sSerialLen = 0;
 }
 ArdUnix::ArdUnix( String lable ):ArdUnixBase(lable),HardwareSerial(){
 	head = tail = NULL;
+	sSerialList = (SoftwareSerial**)new *SoftwareSerial[5];
+	sSerialLen = 0;
 }
 String ArdUnix::sprint( double t ){
 	
 }
 String ArdUnix::sprint( int t ){
 	
+}
+
+bool addApp( String name, String lable, char splict, ArdUnixBase* app, SoftwareSerial* source ){
+	BaseBlock *p = head;
+	while( p != NULL ){
+		if( p->name == name )
+			return ArdUnix.NAME_DUPLICATED;
+		p = p->next;
+	}
+	p = new BaseBlock();
+	if( p == NULL ) return ArdUnix.MEMORY_FAILED;
+	p->name = name;
+	p->lable = lable;
+	p->splict = splict;
+	p->app = app;
+	if( source == NULL )
+		p->source = -1;
+	else{
+		for( int i = 0; i < sSerialLen; i++ ){
+			if( sSerialList[i] == source ){
+				p->source = i;
+				return ArdUnix.SUCCESS;
+			}
+		}
+		if( sSerialLen == 5 )
+			return ArdUnix.SSERIAL_FULL;
+		sSerialList[sSerialLen] = source;
+		p->source = sSerialLen++;
+		return ArdUnix.SUCCESS;
+	}
+}
+
+void ArdUnix::update(){
+	BaseBlock *p = head;
+
+	while( head != null ){
+
+	}
 }
--- ArdUnix.h
+++ ArdUnix.h
@@ -1,6 +1,13 @@
 #ifndef ARDUNIX_H
 #define ARDUNIX_H
 
+#ifndef SUCCESS
+#define SUCCESS 0
+#endif
+
+#define NAME_DUPLICATED 1
+#define 
+
 class ArdUnixBase{
 	private:
 		String lable;
@@ -11,22 +18,40 @@ class ArdUnixBase{
 		virtual void updateRaw( String updStr ) = 0;
 };
 
-typedef struct BaseBlock{
-	String name;
-	String lable;
-	bool withSplict;
-	ArdUnixBase* app;
-	struct BaseBlock* next;
-}BaseBlock;
-
 class ArdUnix : public ArdUnixBase, public HardwareSerial{
-	private:
-		BaseBlock* head, tail;
-	public:
-		ArdUnix();
-		ArdUnix( String lable );
-		String sprint( double t );
-		String sprint( int t );
+private:
+	struct BaseBlock{
+		String name;
+		String lable;
+		char splict;
+		char source;
+		ArdUnixBase* app;
+		struct BaseBlock* next;
+	};
+
+	typedef struct BaseBlock BaseBlock;
+
+	BaseBlock *head, *tail;
+	SoftwareSerial **sSerialList;
+
+	int sSerialLen;
+public:
+	static const char SUCCESS         = 0;
+	static const char NAME_DUPLICATED = 1;
+	static const char MEMORY_FAILED   = 2;
+	static const char SSERIAL_FULL    = 3;
+	// static char SUCCESS = 0;
+	// static char SUCCESS = 0;
+
+	ArdUnix();
+	ArdUnix( String lable );
+
+	bool addApp( String name, String lable, char splict, ArdUnixBase* app, SoftwareSerial* source = null );
+
+	String sprint( double t );
+	String sprint( int t );
+
+	void update();
 };
 
 extern ArdUnix Console;
